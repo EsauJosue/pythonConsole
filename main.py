c7622d79fbaa629ef21e9c95d55d31839c67f5e4
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-01-25 11:25:17 -0600

    Se crea el menu principal de la aplicación y sus opciones correspondientes

--- main.py
+++ main.py
@@ -18,7 +18,15 @@ Acciones disponibles:
 accion = input("¿Qué quieres hacer?: ")
 
 if accion == "registro":
-    print("Ok!! Vamos a registrate en el sistema... ")
+    print("\nOk!! Vamos a registrate en el sistema... ")
+    nombre = input('¿Cual es tu nombre?: ')
+    apellidos = input('¿Cuáles son tus apellidos: ')
+    email = input('Cuál es tu email: ')
+    password = input('Ingresa una contraseña: ')
+
 elif accion == "login":
     print("Vale!! Identifícate en el sistema...")
+    email = input('Cuál es tu email: ')
+    password = input('Ingresa una contraseña: ')
+
 
