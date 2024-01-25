9dd7a3a914f9059a67909bcbba451f8fd8b761dd
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-01-25 11:36:55 -0600

    Se crea el paquete para las acciones que elija el ucuario

---
+++ usuarios/acciones.py
@@ -0,0 +1,11 @@
+class Opciones:
+    def registro(self):
+        print("\nOk!! Vamos a registrate en el sistema... ")
+        nombre = input('¿Cual es tu nombre?: ')
+        apellidos = input('¿Cuáles son tus apellidos: ')
+        email = input('Cuál es tu email: ')
+        password = input('Ingresa una contraseña: ')
+    def login(self):
+        print("Vale!! Identifícate en el sistema...")
+        email = input('Cuál es tu email: ')
+        password = input('Ingresa una contraseña: ')
