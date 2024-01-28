c3892f7d5b6c1ee824dbc661a806a9faa80b4286
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-01-28 10:35:03 -0600

    Se valida si se registró correctamente el usuario se encia mensaje de exito al usuario

--- usuarios/acciones.py
+++ usuarios/acciones.py
@@ -1,3 +1,5 @@
+import usuarios.usuario as modelo
+
 class Opciones:
     def registro(self):
         print("\nOk!! Vamos a registrate en el sistema... ")
@@ -5,6 +7,16 @@ class Opciones:
         apellidos = input('¿Cuáles son tus apellidos: ')
         email = input('Cuál es tu email: ')
         password = input('Ingresa una contraseña: ')
+        
+        usuario = modelo.Usuario(nombre, apellidos, email, password)
+        inscripcion = usuario.registrar()
+        
+        if inscripcion:
+            print(f"Todo ha salido perfecto, se ha registrado el usuario con el email {email}")
+        else: 
+            print("Ocurrio un error en el registro")
+
+    
     def login(self):
         print("Vale!! Identifícate en el sistema...")
         email = input('Cuál es tu email: ')
